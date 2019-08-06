ec476f8731bc86fb3e92811b7c18733c26cfa0a9
Taufique <iradukunda.ta@gmail.com>
2019-08-06 20:43:47 +0200

    add title to admin board and pagination to topics

--- boards/admin.py
+++ boards/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Board, Topic, Post
 
 # Register your models here.
+admin.site.site_header ="Taufique"
 class admin_board(admin.ModelAdmin):
     list_display = ('name','description')
     search_fields = ('name',)
